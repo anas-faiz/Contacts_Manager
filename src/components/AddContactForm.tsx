a3a51fc2c4d0125f79275447819e83ec36ff61d7
Anas-Faiz <anasfaiz0811@gmail.com>
2025-12-06 21:45:27 -0800

    coontact list created

--- src/components/AddContactForm.tsx
+++ src/components/AddContactForm.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
+import ContactList from "./ContactList"
+
 const AddContactForm = ()=>{
 
-    
+    const [name,setName] = useState("")
+    const [contact,setContact] = useState("")
+    const [email,setEmail] = useState("")
+
+    function addContact (){
+        
+    }
+
     return (
         <div className="p-10  ">
             <form className=" flex flex-col gap-1">
-                <input className="border bg-amber-50 p-2" placeholder="Name"></input>
-                <input className="border bg-amber-50 p-2" placeholder="Phone Number"></input>
-                <input className="border bg-amber-50 p-2" placeholder="Email"></input>
-                <button className="border bg-blue-500 p-2 w-1/2 text-center">Add Contact</button>
+                <input value={name} onChange={(e)=>setName(e.target.value)} className="border bg-amber-50 p-2" placeholder="Name"></input>
+                <input value={contact} onChange={(e)=>setContact(e.target.value)} className="border bg-amber-50 p-2" placeholder="Phone Number"></input>
+                <input value={email} onChange={(e)=>setEmail(e.target.value)} className="border bg-amber-50 p-2" placeholder="Email"></input>
+                <button onClick={addContact} className="border bg-blue-500 p-2 w-1/2 text-center">Add Contact</button>
             </form>
+            <ContactList/>
         </div>
     )
 
--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,8 +1,12 @@
 const ContactList = ()=>{
     
     return(
-        <div>
-
+        <div className="border bg-amber-200 m-4">
+            <h1 className="m-1">Name</h1>
+            <h2 className="m-1">Contact Number</h2>
+            <h2 className="m-1">Email</h2>
+            <button className="border bg-blue-400 p-1 mr-2">Edit</button>
+            <button className="border bg-red-400 p-1">Delete</button>
         </div>
     )
 }
